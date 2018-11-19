347bc409eb38b510065936d7f63b65df24bb171b
asingh12541 <asingh@veertrip.com>
2018-11-19 14:51:37 +0530

    echo dependencies

--- replace.js
+++
@@ -1,9 +0,0 @@
-const replace = require("replace");
-
-replace({
-    regex: "master",
-    replacement: process.env.branch,
-    paths: ['./package.json'],
-    recursive: true,
-    silent: true,
-});
